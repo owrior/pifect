fa0e0a2ad2809daaf12a49489a0647c91883956c
owrior <o.w.riordan@gmail.com>
2022-09-05 12:21:23 +0100

    Add test flow.

---
+++ containers/test_flow/flow.py
@@ -0,0 +1,14 @@
+import prefect
+from prefect import flow, task, get_run_logger
+
+
+@task
+def log_task(name):
+    logger = get_run_logger()
+    logger.info("Hello %s!", name)
+    logger.info("Prefect Version = %s 🚀", prefect.__version__)
+
+
+@flow()
+def log_flow(name: str):
+    log_task(name)
